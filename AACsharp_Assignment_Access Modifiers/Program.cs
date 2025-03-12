6a63562c80427f4118a59add7e4bb0a337b8c73f
celalkorucu <21010310079@ogrenci.bartin.edu.tr>
2025-03-12 13:20:38 +0300

    Add project files.

---
+++ AACsharp_Assignment_Access Modifiers/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AACsharp_Assignment_Access_Modifiers;
+
+Console.WriteLine("Hello, World!");
+
+
+User user = new User();
+
+user.setAdSoyad("Celal KORUCU");
+user.setYas(23);
+user.email = "celalkorucu@gmail.com";
+
+user.bilgileriGoster();
+
+// Public : Her yerden erişebileceğimiz değerler
+// Private : Yalnızca bulunduğumumz dosyadaki değerlere erişebilmemizi sağlayan keyword
+// Protected : Sadece tanımlı sınıf ve ondan miraz alan değerler
+// internal : Aynı proje içerisinde yer alanlar erişebilir
+
+// OOP'deki rolü : Erişim belirleyiciler kapsülleme kısmında kullanılır
+// Veri gizliliği ve kod güvenliği sağlar
---
+++ AACsharp_Assignment_Access Modifiers/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AACsharp_Assignment_Access_Modifiers
+{
+    internal class User
+    {
+        private string adSoyad { get; set; }
+        private int yas { get; set; }
+
+        public string email;
+        public void bilgileriGoster()
+        {
+            Console.WriteLine("Kişi ad Soyad : " + adSoyad);
+            Console.WriteLine("Kişi yaş : " + yas);
+            Console.WriteLine("Kişi email : " + email);
+        }
+
+
+        public string getAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void setAdSoyad(string adSoyad)
+        {
+            this.adSoyad = adSoyad;
+        }
+
+
+        public int getYas()
+        {
+            return yas;
+        }
+
+        public void setYas(int yas)
+        {
+            this.yas = yas;
+        }
+    }
+}
